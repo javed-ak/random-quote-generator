449ecf6c4fcbcab534307391fcf5553df7c3e629
Javed Akhtar <conn.javed@gmail.com>
2024-11-20 18:22:42 +0530

    file uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ interface QuoteProps {
   author: string;
 }
 function App() {
+  const [loading, setLoading] = useState(true)
   const [bgColor, setBgColor] = useState("#000000"); 
   const [quoteInfo, setQuoteInfo] = useState<QuoteProps>({
     quote: '',
@@ -26,27 +27,33 @@ function App() {
 
 
   const getQuote = async () => {
-    const response = await axios.get('https://zenquotes.io/api/random')
-    setQuoteInfo({
-      quote: response.data.content,
-      author: response.data.author
+    const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=education', {
+      headers: {
+        'X-Api-Key': import.meta.env.VITE_API_KEY
+      }
     })
+    setQuoteInfo({
+      quote: response.data[0].quote,
+      author: response.data[0].author
+    })
+    setLoading(false)
     changeColor();
   }
+
   return (
     <>
       <div className="h-screen w-screen flex flex-col justify-center items-center transition-colors duration-300" style={{backgroundColor: bgColor}}>
-        <div id='quote-box' className="p-10 shadow-lg rounded-lg lg:max-w-2xl bg-slate-50" style={{color: bgColor}}>
+        <div id='quote-box' className="p-10 shadow-lg rounded-lg lg:max-w-2xl bg-slate-50 mx-5 lg:m-0" style={{color: bgColor}}>
           <div className="flex gap-3">
             <div className=""><FaQuoteLeft size={30}/></div>
-            <div className="text-lg lg:text-4xl font-medium" id='text'>{quoteInfo.quote}</div>
+            <div className="text-lg md:text-2xl lg:text-4xl font-medium font-serif" id='text'>{quoteInfo.quote}</div>
           </div>
           <div className="text-right mt-5" id='author'>- {quoteInfo.author}</div>
           <div className="flex justify-between my-5">
             <div className="flex gap-2 justify-center items-center">
               Share: <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo.quote} target="_blank" id='tweet-quote'><FaSquareXTwitter size={30} /></a>
             </div>
-            <button className="rounded-lg text-slate-50 px-4 py-2" style={{backgroundColor: bgColor}} id='new-quote' onClick={getQuote}>New Quote</button>
+            <button className="rounded-lg text-slate-50 px-4 py-2" style={{backgroundColor: bgColor}} id='new-quote' onClick={getQuote}>{loading ? 'Loading...' : 'New Quote'}</button>
           </div>
         </div>
         <div className="text-slate-50 mt-5 font-light">by <a href="https:x.com/javed__ak">Javed</a></div>
